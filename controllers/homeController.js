2c65b6b94a044c603de4ac38596cd95af9125c46
VeryRusky <russplay0511@gmail.com>
2026-03-18 16:45:14 +0800

    fixed create post, and image uploading
    creating a post now works, and landing will display based on newest post.
    
    image file upload also works without the use of S3 bucket, it works by converting file to binary and storing it into mongo, then we convert it to base64 so the browser can display it

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,10 @@
 const postModel = require('../models/postModel');
-const timeAgo = require("../functions/timeAgo") 
+const timeAgo = require("../functions/timeAgo")
+const mongoose = require('mongoose');
 
 //displayAllPost diplays everything from newest order in the array (added last in the array)
 exports.displayAllPost = async (req, res) => {
-  try{
+  try {
     let posts = await postModel.getAllPost();
     const reversedPosts = posts.slice().reverse() //this reverse line just flips the array so the newst post is at the top
     const username = 'russell_dev'; // replace with sessionID later
@@ -11,26 +12,34 @@ exports.displayAllPost = async (req, res) => {
     const postsWithVotes = reversedPosts.map((post) => {
       //just to test if i up/downvote, whether the button will remain highlighted
       const existingVote = post.voters.find((voter) => voter.username === username);
+
+      //converting the img buffer to base64 string for ejs
+let imageBase64 = null;
+if (post.image && post.image.data) {
+  imageBase64 = Buffer.from(post.image.data.buffer).toString('base64');
+}
       return {
         ...post,
-        userVote: existingVote ? existingVote.voteType : null
+        userVote: existingVote ? existingVote.voteType : null,
+        imageBase64,
+        imageType: post.image ? post.image.contentType : null
       };
     });
     let data = req.query.query
     data = data ? data : undefined
 
     res.render("landing", {
-       posts: postsWithVotes, 
-        query: data,
-        timeAgo
+      posts: postsWithVotes,
+      query: data,
+      timeAgo
     })
   }
   catch (error) {
     console.error(error);
     console.log('Mongoose state:', mongoose.connection.readyState);
-    res.send("Error reading database");
+    res.send("Error reading database " + error.message);
   }
-    
+
 }
 
 // russell old upvote code
@@ -56,7 +65,7 @@ exports.upvote = async (req, res) => {
     }
     await postModel.insertAll(posts);
   }
-    res.redirect(`/home#post-${id}`);
+  res.redirect(`/home#post-${id}`);
 };
 
 exports.downvote = async (req, res) => {
@@ -81,6 +90,6 @@ exports.downvote = async (req, res) => {
     }
     await postModel.insertAll(posts);
   }
-    res.redirect(`/home#post-${id}`);
+  res.redirect(`/home#post-${id}`);
 };
 
